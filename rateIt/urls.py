8603137ae7c6b051cc178c54a2c885fcfc36a91f
cephaske254 <cephaske254@gmail.com>
2020-06-07 13:49:54 +0300

    created accounts app

--- rateIt/urls.py
+++ rateIt/urls.py
@@ -21,6 +21,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('accounts.urls')),
     path('', include('main.urls')),
 ]
 if settings.DEBUG :
